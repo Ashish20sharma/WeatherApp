27529a72affc06afa2f17b1c0a5f88bb295d2052
Ashish20sharma <133103644+Ashish20sharma@users.noreply.github.com>
2024-04-29 23:43:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bce93e41f89eb673d5682b47d9d1be5c`).then((res) => res.json()).then((data) => setWeatherData(data))
+  }, [city])
+  // console.log(weatherData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div class="container">
+        <h1>Weather App</h1>
+        <input type="text" id="locationInput" placeholder="Enter a city" />
+        <button id="searchButton">Search</button>
+        <div class="weather-info">
+          <h2 id="location">Jaipur</h2>
+          <p id="temperature">33</p>
+          <p id="description">hahhhah</p>
+        </div>
+      </div>
+    </body>
   );
 }
 
